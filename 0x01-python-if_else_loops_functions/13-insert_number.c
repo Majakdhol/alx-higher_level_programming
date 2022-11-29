2a6c6d31708254c01d2eab5a013f7d0e1fcafd5a
Majakdhol <majakmarial1996@gmail.com>
2022-11-29 12:45:27 -0800

    Task thirteen

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * insert_node - Insert a number into a sorted singly - linked list
+ * insert_node - insert a number into a sorted singly - linked list
  * @head: A pointer the head of the linked list
  * @number: The number to insert
  *
