5c30aaed974837478223d48d2f6206e34330a279
Majakdhol <majakmarial1996@gmail.com>
2022-11-29 13:05:53 -0800

    Task sixteen

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    "march byecode provided by Holberton school."
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
