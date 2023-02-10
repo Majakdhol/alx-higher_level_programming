305f632367da22d4476adab536c235c8ee04d112
Majakdhol <majakmarial1996@gmail.com>
2023-02-10 05:38:34 -0800

    Task one

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""write_file
+"""write_file module.
+
+Contains a function that writes a text file.
 """
 
-def write_file(filename="", text=""):
-    """Takes str filename to read, and str text to write to
-    """
 
-    with open(filename, mode="w", encoding="utf-8") as writeFile:
-        writeFile.write(text)
-        return len(text)
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and
+    returns the number of characters written.
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)
