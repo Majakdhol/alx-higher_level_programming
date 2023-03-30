5999dd8ee39f83ce5f469a600502c06bf88e64a8
Majakdhol <majakmarial1996@gmail.com>
2023-03-30 13:33:13 -0700

    Advanced tasks

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,5 @@
+ble File  3 lines (3 sloc)  214 Bytes
+
+#!/bin/bash
+# makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response
+curl -o /dev/null -sw "You got me!" 0.0.0.0:5000/catch_me
